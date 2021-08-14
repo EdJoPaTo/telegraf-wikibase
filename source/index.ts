8a886eeffafc41070603d6157e6d4ec5b1d25eeb
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-14 08:24:06 +0200

    refactor(xo): adapt to new rules

--- source/index.ts
+++ source/index.ts
@@ -174,7 +174,7 @@ export class TelegrafWikibase {
 
 		const localeProgress = allEntries
 			.flatMap(o => Object.keys(o.labels ?? {}))
-			// eslint-disable-next-line unicorn/no-array-reduce, @typescript-eslint/prefer-readonly-parameter-types
+			// eslint-disable-next-line unicorn/no-array-reduce, @typescript-eslint/prefer-readonly-parameter-types, unicorn/prefer-object-from-entries
 			.reduce((coll: Record<string, number>, add) => {
 				if (!coll[add]) {
 					coll[add] = 0;
